d35b37cac890afaa3f6a6cfe988328edc1d6975a
robinrestrepo23 <rrestrepoamaya23@gmail.com>
2026-06-11 17:29:38 -0500

    Cambios

--- pos-restaurante/src/test/java/com/restaurant/pos_restaurante/PosRestauranteApplicationTests.java
+++ pos-restaurante/src/test/java/com/restaurant/pos_restaurante/PosRestauranteApplicationTests.java
@@ -3,11 +3,11 @@ package com.restaurant.pos_restaurante;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+/* @SpringBootTest
 class PosRestauranteApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
-}
+} */
